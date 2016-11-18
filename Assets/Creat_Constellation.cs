bff839cbefcd11aa16f37a1a84b3955af7d25172
Mo <taomo2@illinois.edu>
2016-11-18 16:31:38 -0600

    Nov16

---
+++ Assets/Creat_Constellation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+public class Creat_Constellation : MonoBehaviour {
+    // Use this for initialization
+    public long N = 7000;
+    public long M = 10000;
+    public float scale = 1f;
+    public Boolean mat = true;
+    void Start()
+    {
+        Material newMat = Resources.Load("Sun", typeof(Material)) as Material;
+
+        GameObject[] sphere = new GameObject[N];
+        long Count = 0;
+
+        System.Random rdm = new System.Random();
+
+        var readerX = new StreamReader(File.OpenRead(@"./Assets/Resources/X.txt"));
+        var readerY = new StreamReader(File.OpenRead(@"./Assets/Resources/Y.txt"));
+        var readerZ = new StreamReader(File.OpenRead(@"./Assets/Resources/Z.txt"));
+        //int i = 0;
+        while (!readerX.EndOfStream && Count < N)
+        {
+            float tx = ReadLine(readerX);
+            float ty = ReadLine(readerY);
+            float tz = ReadLine(readerZ);
+
+            if (mat)
+            {
+                String name = "Sunpre" + rdm.Next(0, 3).ToString();
+                sphere[Count] = (GameObject)Instantiate(Resources.Load(name, typeof(GameObject)));
+            }
+            else
+            {
+                sphere[Count] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            }
+            sphere[Count].transform.position = new Vector3(tx * M, ty * M, tz * M);
+            sphere[Count].transform.localScale = new Vector3(scale, scale, scale);
+            Count++;
+
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    float ReadLine(StreamReader reader)
+    {
+        var line = reader.ReadLine();
+        var values = line.Split(';');
+        return Convert.ToSingle(values[0]);
+    }
+}
---
+++ Assets/Walker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walker : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public Camera MainCam;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        transform.Translate(MainCam.transform.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(MainCam.transform.right *-1* speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(MainCam.transform.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(MainCam.transform.forward * -1 * speed * Time.deltaTime);
+    }
+}
